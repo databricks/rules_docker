9a86e27d5da17f8eed65b50ee9373ddea44dffc1
Li Haoyi <haoyi.li@databricks.com>
2023-10-10 00:07:00 -0700

    .

--- container/force_py2.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-if __name__ == '__main__':
-    argv = sys.argv
-    path, _ = os.path.split(argv[0])
-    argv[0] = os.path.join(path, 'build_tar.py')
-    argv = ['/usr/bin/env', 'python2'] + argv
-    os.execv(argv[0], argv)
